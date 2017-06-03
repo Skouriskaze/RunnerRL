4e4149be1b4a6d05f8a5b4d7178445fd097cf154
skouriskaze <skouriskaze@gmail.com>
2017-06-03 14:55:45 -0400

    First commit

---
+++ RunnerRL/Runner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunnerRL
+{
+    public partial class Runner : Form
+    {
+        private Graphics graphics;
+        public Runner()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics();
+            testRectangle();
+        }
+
+        public void testRectangle()
+        {
+            Rectangle r = new Rectangle(0, this.Bottom - 10, this.Width, 10); 
+        }
+
+    }
+}
